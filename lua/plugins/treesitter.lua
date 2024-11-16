571c1e5721ae64a6be851f47850fd6cd98bb448c
Piotr Fert <fert.piotr@gmail.com>
2024-11-17 00:43:52 +0100

    initial commit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,13 @@ return {
 			ensure_installed = { "lua", "javascript", "java", "groovy", "html", "jsdoc", "typescript", "angular" },
 			highlight = { enable = true },
 			indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    node_incremental = "v",
+                    node_decremental = "V",
+                }
+            }
 		})
 	end,
 }
