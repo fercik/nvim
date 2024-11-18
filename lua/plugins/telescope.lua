792caaed9592e812e4017ee612bac13c3e7b1364
Piotr Fert <fert.piotr@gmail.com>
2024-11-18 01:44:09 +0100

    added live grep to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
 			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		end,
 	},
 	{
