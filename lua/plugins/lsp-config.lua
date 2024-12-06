e25f0251896190035534a862df82f7129501f56b
Piotr Fert <fert.piotr@gmail.com>
2024-12-06 15:14:44 +0100

    harpoon

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,78 +1,80 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            PATH = "prepend",
-        },
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "ts_ls",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "nvim-java/nvim-java",
-        },
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            require("java").setup({
-                jdk = {
-                    auto_install = false,
-                },
-            })
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			PATH = "prepend",
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"angularls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"nvim-java/nvim-java",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.jdtls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.emmet_language_server.setup({
-                capabilities = capabilities,
-                filetypes = {
-                    "css",
-                    "html",
-                    "javascript",
-                    "scss",
-                    "typescript",
-                },
-                -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-                -- **Note:** only the options listed in the table are supported.
-                init_options = {
-                    ---@type table<string, string>
-                    includeLanguages = {},
-                    --- @type string[]
-                    excludeLanguages = {},
-                    --- @type string[]
-                    extensionsPath = {},
-                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-                    preferences = {},
-                    --- @type boolean Defaults to `true`
-                    showAbbreviationSuggestions = true,
-                    --- @type "always" | "never" Defaults to `"always"`
-                    showExpandedAbbreviation = "always",
-                    --- @type boolean Defaults to `false`
-                    showSuggestionsAsSnippets = false,
-                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-                    syntaxProfiles = {},
-                    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-                    variables = {},
-                },
-            })
+			require("java").setup({
+				jdk = {
+					auto_install = false,
+				},
+			})
 
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.emmet_language_server.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"css",
+					"html",
+					"javascript",
+					"scss",
+					"typescript",
+				},
+				-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+				-- **Note:** only the options listed in the table are supported.
+				init_options = {
+					---@type table<string, string>
+					includeLanguages = {},
+					--- @type string[]
+					excludeLanguages = {},
+					--- @type string[]
+					extensionsPath = {},
+					--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+					preferences = {},
+					--- @type boolean Defaults to `true`
+					showAbbreviationSuggestions = true,
+					--- @type "always" | "never" Defaults to `"always"`
+					showExpandedAbbreviation = "always",
+					--- @type boolean Defaults to `false`
+					showSuggestionsAsSnippets = false,
+					--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+					syntaxProfiles = {},
+					--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+					variables = {},
+				},
+			})
+
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
