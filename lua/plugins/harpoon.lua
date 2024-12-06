5ec0978f1d43a52386b401196d785599f9fa9b0e
Piotr Fert <fert.piotr@gmail.com>
2024-12-06 15:16:49 +0100

    harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,18 +1,18 @@
 return {
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local harpoon = require("harpoon")
-			harpoon.setup({})
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon.setup({})
 
-			vim.keymap.set("n", "<leader>h", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end)
-			vim.keymap.set("n", "<C-h>", function()
-				harpoon:list():add()
-			end)
-		end,
-	},
+            vim.keymap.set("n", "<leader>h", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end)
+            vim.keymap.set("n", "<leader>hh", function()
+                harpoon:list():add()
+            end)
+        end,
+    },
 }
