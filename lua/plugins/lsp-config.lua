ccc098c85a183d27652fa067b2b892e5de027baf
Piotr Fert <fert.piotr@gmail.com>
2025-09-04 01:51:07 +0200

    feat: added blink.cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,7 +29,7 @@ return {
 				"force",
 				{},
 				vim.lsp.protocol.make_client_capabilities(),
-				require("cmp_nvim_lsp").default_capabilities()
+				require("blink.cmp").get_lsp_capabilities({}, false)
 			)
 
 			vim.lsp.enable("angularls", {
