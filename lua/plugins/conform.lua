34a24dabd2ff07a8fecf3eb28727e34510422976
fercik <fert.piotr@gmail.com>
2025-09-04 16:02:05 +0200

    feat: formatters for styles

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,10 +1,12 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-            typescript = { "prettierd", "prettier", stop_after_first = true },
-        },
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
+			scss = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
 }
