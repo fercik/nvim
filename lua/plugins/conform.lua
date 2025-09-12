05727e7332979b6275cdd666fe5fc2f5c73893fe
fercik <fert.piotr@gmail.com>
2025-09-12 17:29:06 +0200

    feat: angular prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
 			css = { "prettierd", "prettier", stop_after_first = true },
 			scss = { "prettierd", "prettier", stop_after_first = true },
 			html = { "prettierd", "prettier", stop_after_first = true },
+			htmlangular = { "prettierd", "prettier", stop_after_first = true },
 		},
 	},
 }
